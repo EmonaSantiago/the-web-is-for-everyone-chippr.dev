e89ab632afa7bce8e0f2ad9ee6d834b718fd3201
Emona Santiago <emonasantiago1207@gmail.com>
2022-04-06 13:46:05 +0200

    api added

--- index.js
+++ index.js
@@ -1,74 +1,19 @@
-// console. log('hello world 👋')
-
-// process.on('exit', function(){
-
-// })
-
-// const { EventEmitter } = require('events');
-// const eventEmitter = new EventEmitter();
-
-// eventEmitter.on('lunch',() => {
-
-//     console.log('Yummy 🍚')
-// })
-
-// Registering for the event with callback parameters
-// eventEmitter.on('lunch', (code, msg)=> console.log(`Got ${code} and ${msg}`));
-
-// eventEmitter.emit('lunch');
-// eventEmitter.emit('lunch');
-// eventEmitter.emit('lunch', 200, 'ok');
-
-// const { readFile, readFileSync } = require('fs');
-
-// const txt = readFileSync ('./hello.txt', 'utf8');
-// console.log(txt)
-
-// console.log('Im also here Emona')
-
-// make your code non blocking. switch up consoles. Ook al de console eerder in scriopt wordt de ander eerst aangegeven. 
-// readFile('./hello.txt', 'utf8', (err, txt) => {
-//     console.log(txt)
-
-// })
-
-// Promisses; async, non blocking
-// const { readFile } = require('fs').promises;
-
-// async function hello() {
-//     const file = await readFile('./hello.txt', 'utf8');
-// }
-
-
-
-// const myModule = require('./my-module');
-
-// console.log(myModule)
-
-// const { readFile } = require('fs').promises;
-
-// app.get('./', async (request, response) => {
-
-//     response.send( await readFile('./home.html', 'utf8') );
-
-// });
-
 const express = require('express');
-const { read, readFile } = require('fs');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express(); 
+const url = "https://chipr.api.fdnd.nl/v1/project"
 
-app.get('/', (request, response) => {
+app.get('/projects', (request, response) => {
+    //1. Fetch projects from chippr API
      
-    readFile('./index.html', 'utf-8', (err, html) => {
-
-        if (err) {
-            response.status(500).send('sorry, out of order')
-        }
-
-        response.send(html)
-
-    })
+    fetch(url)
+     .then(response => response.json())
+     .then(data => {
+        //2. Render results in Html
+        response.render(data)
+     })
+     .catch(err => console.log(err))
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`App avaiable on http:localhost:3000`))
