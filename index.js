d5f9c39950c62389861bb51f7bda2e38c81d6e6e
Emona Santiago <emonasantiago1207@gmail.com>
2022-04-13 09:44:34 +0200

    added css

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
@@ -5,23 +6,36 @@ const app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 const url = "https://chipr.api.fdnd.nl/v1/project"
-
-app.get("/projects", (request, response) => {
+app.use(express.static(__dirname + '/public'));
+ 
+app.get("/", (request, response) => {
      //1. Fetch projects from chippr API
      fetch(url)
      .then(response => response.json())
      .then(projects => {
           //2. Render results in Html
           console.log(projects)
-          response.render('projects', {projects: projects.data})
+          response.render('index', {projects: projects.data})
      })
      .catch(err => console.log(err))
 
 });
 
+app.get("/:id", (request, response) => {
+     fetch(url)
+     .then(response => response.json())
+     .then(projects => {
+          //2. Render results in Html
+          console.log(projects)
+          response.render('detail', {projects: projects.data[0],
+          })
+     })
+     .catch(err => console.log(err))
+
+});
+
+
 app.listen(process.env.PORT || 3000, () => console.log(`App avaiable on http://localhost:3000`)) 
-
-
 
 
 
